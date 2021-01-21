645dee08a489a0825295111e97a9ab6e9427964e
cpviolator <dmhowarth26@gmail.com>
2021-01-21 15:25:08 -0800

    Edit launcher

--- wilson/launcher.sh
+++ wilson/launcher.sh
@@ -2,29 +2,24 @@
 
 export OMP_NUM_THREADS=4
 
-# Simple test script to demonstrate how to use the 2D U(1) code
-# The size of the lattice (L) is hardcoded in the main.cpp file
-# to make writing new code simpler. Please please edit and remake
-# if you wish to vary L.
-
 mkdir -p {gauge,data/{data,plaq,creutz,polyakov,rect,top,pion,vacuum,eig}}
 
 # The value of the coupling in the U(1) 2D theory
-BETA=5.0
+BETA=15.0
 
 # The total number of HMC iterations to perform.
-HMC_ITER=1000
+HMC_ITER=2000
 # The number of HMC iterations for thermalisation.
-HMC_THERM=50
+HMC_THERM=250
 
 # The number of HMC iterations to skip bewteen measurements.
 HMC_SKIP=10
 # Dump the gauge field every HMC_CHKPT iterations after thermalisation.
-HMC_CHKPT=10
+HMC_CHKPT=20
 # If non-zero, read in the HMC_CHKPT_START gauge field. 
 HMC_CHKPT_START=0
 # HMC time steps in the integration 
-HMC_NSTEP=15
+HMC_NSTEP=20
 # HMC trajectory time
 HMC_TAU=1.0
 
@@ -41,17 +36,17 @@ DYN_QUENCH=1
 
 # Dynamic fermion parameters
 # Fermion mass
-MASS=0.32
+MASS=0.01
 # Maximum CG iterations
 MAX_CG_ITER=10000
 # CG tolerance
 CG_EPS=1e-16
 
 # Eigensolver parameters
-DEFLATE=1
-NKR=48
-NEV=16
-NCONV=16
+DEFLATE=0
+NKR=128
+NEV=96
+NCONV=96
 
 # Tolerance on the residual
 EIG_TOL=1e-10
@@ -66,7 +61,7 @@ N_POLY=100
 
 X_BLK=4
 Y_BLK=4
-N_LOW=12
+N_LOW=16
 NDEFL=${N_LOW}
 
 # Measuremets: 1 = measure, 0 = no measure
@@ -79,13 +74,14 @@ MEAS_PC=1
 # Vacuum trace
 MEAS_VT=1
 
-LX=8
-LY=8
+LX=16
+LY=16
 
 command="./wilson2D $BETA $HMC_ITER $HMC_THERM $HMC_SKIP $HMC_CHKPT 
          $HMC_CHKPT_START $HMC_NSTEP $HMC_TAU $APE_ITER $APE_ALPHA $RNG_SEED 
 	 $DYN_QUENCH $MASS $MAX_CG_ITER $CG_EPS $DEFLATE $NKR $NEV $NCONV 
-	 $EIG_TOL $MAXITER $USE_ACC $AMAX $AMIN $N_POLY $X_BLK $Y_BLK $N_LOW $NDEFL $LX $LY"
+	 $EIG_TOL $MAXITER $USE_ACC $AMAX $AMIN $N_POLY $X_BLK $Y_BLK $N_LOW $NDEFL 
+	 $MEAS_PC $MEAS_WL $LX $LY"
 
 echo $command
 
