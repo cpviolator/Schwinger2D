2306aef746d4a88dc334d86e31bc89ff2c204b3c
Dean Howarth <2357719+cpviolator@users.noreply.github.com>
2022-05-19 14:42:45 -0700

    Update dirac_op.cpp

--- lib/dirac_op.cpp
+++ lib/dirac_op.cpp
@@ -1,5 +1,13 @@
 #include "dirac_op.h"
 
+// Wilson stencil                                                                                                                                                                 
+// D_{W}(n,m) = (m_{0} + 2r)\delta(n,m)                                                                                                                                           
+//               - 1/2 Sum [(r-\sigma_{mu}) U_{n,\mu} \delta_{n,m-\hat{\mu}} +                                                                                                    
+//                          (r+\sigma_{mu}) U^{\dagger}_{m,\mu} \delta_{n,m+\hat{\mu}}]                                                                                           
+//                                                                                                                                                                                
+// sigma_1 = | 0  1 |  sigma_2 = | 0 -i | sigma_3 = i*sigma_1*sigma_2 = | 1  0 |                                                                                                  
+//           | 1  0 |            | i  0 |                               | 0 -1 |                                                                                                  
+
 void Dpsi(field<Complex> *out, const field<Complex> *in, const field<Complex> *gauge) {
   
   double m0 = gauge->p.m;
